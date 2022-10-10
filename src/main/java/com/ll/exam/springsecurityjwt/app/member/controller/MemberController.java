c0f1dd086a636686a5a034f07b8aff8d5a827e28
sh970901 <aaddss639@naver.com>
2022-10-10 17:53:46 +0900

    resultCode, msg ,data가 포함되도록 강제

--- src/main/java/com/ll/exam/springsecurityjwt/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/springsecurityjwt/app/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ll.exam.springsecurityjwt.app.member.controller;
 
+import com.ll.exam.springsecurityjwt.app.base.dto.RsData;
 import com.ll.exam.springsecurityjwt.app.member.entity.Member;
 import com.ll.exam.springsecurityjwt.app.member.service.MemberService;
 import com.ll.exam.springsecurityjwt.util.Util;
@@ -21,24 +22,24 @@ public class MemberController {
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<RsData> login(@RequestBody LoginDto loginDto) {
         if(loginDto.isNotValid()){
-            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+            return Util.spring.responseEntityOf(RsData.of("F-1", "로그인 정보가 올바르지 않습니다."));
         }
         Member member = memberService.findByUsername(loginDto.getUsername()).orElse(null);
 
         if (member == null) {
-            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+            return Util.spring.responseEntityOf(RsData.of("F-2", "일치하는 회원이 존재하지 않습니다."));
         }
 
         if (passwordEncoder.matches(loginDto.getPassword(), member.getPassword()) == false) {
-            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+            return Util.spring.responseEntityOf(RsData.of("F-3", "비밀번호가 일치하지 않습니다."));
         }
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authentication", "JWT_Access_Token");
 //        return new ResponseEntity<>(body, headers, HttpStatus.OK);
-        return Util.spring.responseEntityOf(headers);
+        return Util.spring.responseEntityOf(RsData.of("S-1", "로그인 성공, Access Token을 발급합니다."), headers);
     }
 
     @Data
