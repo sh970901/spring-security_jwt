4f982ee573a4f3817b5cd34958d96934add29ac2
sh970901 <aaddss639@naver.com>
2022-12-01 20:43:47 +0900

    주석처리

--- src/main/java/com/ll/exam/springsecurityjwt/app/member/security/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/ll/exam/springsecurityjwt/app/member/security/filter/JwtAuthorizationFilter.java
@@ -34,6 +34,8 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         log.debug("JwtAuthorizationFilter 실행됨");
         String bearerToken = request.getHeader("Authorization");
 
+        //토큰은 폐기가 불가능하니 처음에는 리프레쉬 토큰을 썼지만 유효기간을 짧게 두고 계속 보내는 방식이 클라이언트 쪽에서 할일이 많아질거같다고 느꼈습니다. 
+        //모든 요청 보낼때마다 로그인이 풀렸는지 토큰이 만료되었는지 체크하는게 불편
         if (bearerToken != null) {
             String token = bearerToken.substring("Bearer ".length());
             // 1차 체크(정보가 변조되지 않았는지 체크)
@@ -42,6 +44,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                 // 캐시(레디스)를 통해서
                 Member member = memberService.getByUsername__cached((String) claims.get("username"));
                 // 2차 체크(화이트리스트에 포함되는지)
+                // 블랙리스트는 문제가 되는 애들을 기록, 화이트리스트는 나머지를 다 이상한애로 보고 오직 유효한것만
                 if ( memberService.verifyWithWhiteList(member, token) ) {
                     //로그인 처리
                     forceAuthentication(member);
